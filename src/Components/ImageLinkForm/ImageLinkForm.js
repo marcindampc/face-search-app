0f4ddf7108bd50dae13979080c6fc76c19852334
Marcin Dampc <marcindampc@gmail.com>
2019-08-01 02:45:46 +0100

    submit image form created

--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import './ImageLinkForm.css';
 
 const ImageLinkForm = () => {
 return (
   <div>
-
+    <p className='f3'>
+      {'This app will automaticly detect face in your pictures. Try it!'}
+    </p>
+    <div className='center'>
+      <div className='pa4 br3 shadow-5 center form'>
+        <input className='f4 pa2 w-70 center' type='text'/>
+        <button className='w-30 grow f4 link ph3 pv2 dib white bg-green'>Detect</button>
+      </div>
+    </div>
   </div>
 )
 }

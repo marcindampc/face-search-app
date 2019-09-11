31bb45bdf30cf60befe494e6a314b38c016bb325
Marcin Dampc <marcindampc@gmail.com>
2019-09-11 07:48:59 +0100

    apply submit method to Register component

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -22,9 +22,28 @@ class Register extends React.Component {
     this.setState({password: event.target.value})
   }
 
-  render() {
-    const { onRouteChange } = this.props;
+  onSubmitSignIn = () => {
+    fetch('http://localhost:3000/signin', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.password,
+        name: this.state.name
+      })
+    })
+    .then(response => response.json())
+    .then(user  => {
+      if (user) {
+        this.props.onRouteChange('home');
+      }
+    })
 
+  }
+
+  render() {
     return(
       <article className="mw6 center br4 pa3 pa4-ns mv4 w-100 w-50-m w-25-1 ba b--white-10 shadow-5">
         <main className="pa4 black-80">
@@ -33,22 +52,42 @@ class Register extends React.Component {
               <legend className="f1 fw6 ph0 mh0">Register</legend>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
-                <input className="pa2 input-reset ba bg-transparent hover-white w-100" type="text" name="name" id="name" />
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-white w-100"
+                  type="text"
+                  name="name"
+                  id="name"
+                  onChange={this.onNameChange}
+                />
               </div>
               <div className="mt3">
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-                <input className="pa2 input-reset ba bg-transparent hover-white w-100" type="email" name="email-address" id="email-address" />
+                <input
+                  className="pa2 input-reset ba bg-transparent hover-white w-100"
+                  type="email"
+                  name="email-address"
+                  id="email-address"
+                  onChange={this.onEmailChange}
+                />
               </div>
               <div className="mv3">
                 <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-white w-100" type="password" name="password" id="password" />
+                <input
+                  className="b pa2 input-reset ba bg-transparent hover-white w-100"
+                  type="password"
+                  name="password"
+                  id="password"
+                  onChange={this.onPasswordChange}
+                />
               </div>
             </fieldset>
             <div className="">
               <input
-                onClick={() => onRouteChange('home')}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit"
-                value="Register" />
+                onClick={() => this.onSubmitSignIn}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                type="submit"
+                value="Register"
+              />
             </div>
           </div>
         </main>
--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -34,7 +34,7 @@ class SignIn extends React.Component {
         this.props.onRouteChange('home');
       }
     })
-    
+
   }
 
   render(){

a39a23b123a0980cc21f5c19219e15a1d0987f0c
Marcin Dampc <marcindampc@gmail.com>
2019-09-12 05:37:49 +0100

    change fetch route for Register, extend App state with user, pass attributes to Rank

--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Rank = () => {
-return (
+const Rank = ({ name, entries }) => {
+  return (
   <div>
    <div className='white f3'>
-    {'Marcin, your current rank is:'}
+    {`${name}, your current rank is:`}
    </div>
    <div className='white f1'>
-    {'#2'}
+    {`#${entries}`}
    </div>
   </div>
 )
--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('http://localhost:3000/signin', {
+    fetch('http://localhost:3000/register', {
       method: 'post',
       headers: {
         'Content-Type': 'application/json'
@@ -37,10 +37,10 @@ class Register extends React.Component {
     .then(response => response.json())
     .then(user  => {
       if (user) {
+        this.props.loadUser(user);
         this.props.onRouteChange('home');
       }
     })
-
   }
 
   render() {
@@ -83,7 +83,7 @@ class Register extends React.Component {
             </fieldset>
             <div className="">
               <input
-                onClick={() => this.onSubmitSignIn}
+                onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
